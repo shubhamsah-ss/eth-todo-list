f5835fa3b2002ef50649d7f97de247845eb32191
shubhamsah-ss <shubham.6552@gmail.com>
2023-05-05 11:10:25 +0530

    3. Create Tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -16,9 +16,17 @@ contract TodoList {
 
   mapping(uint => Task) public tasks;
 
+
+  event TaskCreated (
+    uint id,
+    string content,
+    bool completed
+  );
+
   function createTask(string memory _content) public {
     taskCount++;
     tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
   }
 
   
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -21,4 +21,16 @@ contract (TodoList, (accounts) => {
     assert.equal(task.completed, false)
     assert.equal(taskCount.toNumber(), 1)
   })
+
+  it('create tasks', async () => {
+    const result = await this.todoList.createTask('eth-todo-list')
+    const taskCount = await this.todoList.taskCount()
+    assert.equal(taskCount, 2)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 2)
+    assert.equal(event.content, 'eth-todo-list')
+    assert.equal(event.completed, false)
+  })
+
+  
 })
